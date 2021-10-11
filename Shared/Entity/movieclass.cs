717af34030c474711dc48e5d26f35230a105ab41
Mikedrp <mikedelriopinilla@gmail.com>
2021-10-11 11:42:32 -0500

    Proyecto Smart Tech V1.0

---
+++ Shared/Entity/movieclass.cs
@@ -0,0 +1,9 @@
+namespace ProyPeliculas_G63.Shared.Entity
+{
+    public class movieclass
+    {
+        public string Movie{get;set;}
+        public string sipnosis{get;set;}
+        public DateTime Premier{get;set;}
+    }
+}
